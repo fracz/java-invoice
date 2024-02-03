8a5b95dd89e6c71c992a438f0032dc61a31ede38
antmazur <sonicmephit@gmail.com>
2024-02-03 13:54:44 +0100

    Update tests

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -1,6 +1,9 @@
 package pl.edu.agh.mwo.invoice.product;
 
+import org.hamcrest.Matchers;
+
 import java.math.BigDecimal;
+import java.math.BigInteger;
 
 public abstract class Product {
     private final String name;
@@ -10,24 +13,25 @@ public abstract class Product {
     private final BigDecimal taxPercent;
 
     protected Product(String name, BigDecimal price, BigDecimal tax) {
+        if (name.isEmpty() | name.equals("")) throw new IllegalArgumentException();
         this.name = name;
         this.price = price;
         this.taxPercent = tax;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public BigDecimal getPrice() {
-        return null;
+        return price;
     }
 
     public BigDecimal getTaxPercent() {
-        return null;
+        return taxPercent;
     }
 
     public BigDecimal getPriceWithTax() {
-        return null;
+        return price.multiply(taxPercent).add(price);
     }
 }
