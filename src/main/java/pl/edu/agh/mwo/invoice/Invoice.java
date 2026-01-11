ec25fe33dc464c9c1d2a5ec86894151f93837e64
apostol <apostol@d17-426b-pc-14.ipa.ii.agh.edu.pl>
2026-01-11 15:56:13 +0100

    messing up Invoice.java

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -3,42 +3,62 @@ package pl.edu.agh.mwo.invoice;
 import pl.edu.agh.mwo.invoice.product.Product;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.stream.IntStream;
+import java.util.HashMap;
 import java.util.stream.Stream;
 
 public class Invoice {
-    private final Collection<Product> products = new ArrayList<>();
+//    private final Collection<Product> products = new ArrayList<>();
+
+    HashMap<Product, Integer> products = new HashMap<>();
+
 
     public void addProduct(Product product) {
-        if (product == null) {
-            throw new IllegalArgumentException("Product can't be null!");
-        }
-        products.add(product);
+        addProduct(product, 1);
     }
 
     public void addProduct(Product product, Integer quantity) {
+        if (product == null) {
+            throw new IllegalArgumentException("Product can't be null!");
+        }
         if (quantity < 1) {
             throw new IllegalArgumentException("Number of products can't be negative!");
         }
-        IntStream.range(0, quantity).forEach((p) -> addProduct(product));
+        products.put(product, quantity);
     }
 
-    private BigDecimal add(Stream<BigDecimal> stream) {
+    private BigDecimal reduce(Stream<BigDecimal> stream) {
         return stream.reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 
     public BigDecimal getSubtotal() {
-        return add(products.stream().map(Product::getPrice));
+
+        BigDecimal sum = BigDecimal.ZERO;
+        for (Product product : products.keySet()) {
+            BigDecimal price = product.getPrice();
+            BigDecimal taxPercent = product.getTaxPercent();
+            sum.add(price.multiply(taxPercent));
+        }
+        return sum;
+//        return add(products.keySet().stream()
+//                .map(p -> p.getPrice().multiply(BigDecimal.valueOf(products.get(p)))));
     }
 
     public BigDecimal getTax() {
         return getTotal().subtract(getSubtotal());
-
     }
 
     public BigDecimal getTotal() {
-        return add(products.stream().map(Product::getPriceWithTax));
+        BigDecimal sum = BigDecimal.ZERO;
+        for (Product product : products.keySet()) {
+            BigDecimal priceWithTax = product.getPriceWithTax();
+            BigDecimal taxPercent = product.getTaxPercent();
+            sum = sum.add(priceWithTax.multiply(taxPercent));
+        }
+        return sum;
+
+//        return add(products.keySet().stream()
+//                .map(p -> p.getTaxPercent().multiply(BigDecimal.valueOf(products.get(p)))
+//                )
+//        );
     }
 }
