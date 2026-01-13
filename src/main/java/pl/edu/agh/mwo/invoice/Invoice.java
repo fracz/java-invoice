186ccc2f94a60cfe1f680061f51c43f804b494c5
Anna Maywald <aniamaywald@gmail.com>
2026-01-13 14:47:08 +0100

    No comiit made it through in class... Need to upload all changes made in class in one commit now.

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,30 +1,63 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
     private Collection<Product> products;
 
-    public void addProduct(Product product) {
-        // TODO: implement
+    public Invoice() {
+        this.products = new ArrayList<>();
     }
+
+
+    public void addProduct(Product product) {
+        this.addProduct(product,1);
+    }
+
 
     public void addProduct(Product product, Integer quantity) {
-        // TODO: implement
+        this.productsMap.put(product,quantity);
     }
 
-    public BigDecimal getSubtotal() {
-        return null;
+
+    private Map<Product,Integer> productsMap = new HashMap<>();
+
+    public BigDecimal getNetValue() {
+        BigDecimal netValue = BigDecimal.ZERO;
+
+        for (Product product : this.productsMap.keySet()) {
+            Integer quantity = this.productsMap.get(product);
+            BigDecimal price = product.getPrice();
+            price = price.multiply(BigDecimal.valueOf(quantity));
+            netValue = netValue.add(price);
+
+        }
+        return netValue;
     }
+
 
     public BigDecimal getTax() {
-        return null;
+        return getGrossValue().subtract(getNetValue());
     }
 
-    public BigDecimal getTotal() {
-        return null;
+
+    public BigDecimal getGrossValue() {
+        BigDecimal netValue = BigDecimal.ZERO;
+
+        for (Product product : this.productsMap.keySet()) {
+            Integer quantity = this.productsMap.get(product);
+            BigDecimal price = product.getPriceWithTax();
+            price = price.multiply(BigDecimal.valueOf(quantity));
+            netValue = netValue.add(price);
+
+        }
+        return netValue;
     }
 }
+
